5c8e8ed712f8ae30757e85d86f9090715bfa6cc7
Rafael CDS <rafael.candidodasilva.pro@gmail.com>
2023-06-13 17:52:45 +0200

    add homepage

---
+++ templates/homepage.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+<body>
+    <header>
+        <a href="homepage.php" class="homepage"><!-- image du logo --></a>
+        <label for="search">Rechercher</label>
+        <input type="text" class="search" name="search">
+        <button><!-- icone font awesome loupe --></button>
+        <button type="submit"><!-- menu burger --></button>
+    </header>
+    <main>
+        
+    </main>
+</body>
+</html>
