56c21067f96cecf14a25974d3a0ba4080fec2c61
Rafael CDS <rafael.candidodasilva.pro@gmail.com>
2023-06-13 17:46:06 +0200

    database connection

---
+++ lib/database.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Application\Lib\Database;
+
+class Database
+{
+    public ?\PDO $database = null;
+
+    public function getConnection(): \PDO
+    {
+        if ($this->database === null) {
+            $this->database = new \PDO('mysql:host=localhost;dbname=ustube;charset=utf8', 'root', 'root');
+        }
+
+        return $this->database;
+    }
+}
